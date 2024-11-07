a07d7b1161912f7ed41948989c50602749aedd55
Minghao Li <minghao.li@thoughtworks.com>
2024-11-07 10:54:40 +0800

    [Stream] feat, init repo

---
+++ src/main/java/StreamFilter.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+public class StreamFilter {
+
+    public List<Integer> filterEven(List<Integer> numbers) {
+        return null;
+    }
+
+    public List<String> filterEndWithS(List<String> words) {
+        return null;
+    }
+
+    public List<Integer> getCommonElements(List<Integer> numbers, List<Integer> anotherNumbers) {
+        return null;
+    }
+
+
+}
---
+++ src/main/java/StreamMap.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamMap {
+
+    private static final List<String> ALPHABET = Arrays.asList(
+        "a", "b", "c", "d", "e", "f", "g",
+        "h", "i", "j", "k", "l", "m", "n",
+        "o", "p", "q", "r", "s", "t", "u",
+        "v", "w", "x", "y", "z");
+
+    public List<Integer> getTriple(List<Integer> numbers) {
+        return null;
+    }
+
+    public List<String> mapLetter(List<Integer> numbers) {
+        return null;
+    }
+
+    public List<Integer> mapLength(List<String> words) {
+        return null;
+    }
+}
