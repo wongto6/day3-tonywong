ec6fa38cdfbad39cee94d166e3d43e5c92d0c118
WONGTO6 <wongto6@oocl.com>
2024-11-20 14:07:32 +0800

    refactor: format codes

--- src/main/java/Input.java
+++ src/main/java/Input.java
@@ -2,9 +2,9 @@ public class Input {
     private String value;
     private int count;
 
-    public Input(String w, int i){
-        this.value =w;
-        this.count =i;
+    public Input(String w, int i) {
+        this.value = w;
+        this.count = i;
     }
 
 
