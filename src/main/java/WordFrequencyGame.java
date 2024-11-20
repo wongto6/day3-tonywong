bef234eb51a228fc6c20b6f1837f9a0d7079739e
WONGTO6 <wongto6@oocl.com>
2024-11-20 15:16:43 +0800

    refactor: extract methods from getWordFrequency

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -11,24 +11,37 @@ public class WordFrequencyGame {
     public String getWordFrequency(String sentences) {
         try {
             //split the WordCount string with 1 to n pieces of spaces
-            List<WordFrequency> wordFrequencies = Arrays
-                    .stream(sentences.split(SPACE))
-                    .map(word -> new WordFrequency(word, 1))
-                    .collect(Collectors.toList());
+            List<WordFrequency> wordFrequencies = mapSentenceToWordFrequencies(sentences);
             //get the map for the next step of sizing the same word
             Map<String, List<WordFrequency>> wordToWordFrequencies = getListMap(wordFrequencies);
-
-            wordFrequencies = wordToWordFrequencies
-                    .entrySet()
-                    .stream()
-                    .map(entry -> new WordFrequency(entry.getKey(), entry.getValue().size()))
-                    .collect(Collectors.toList());
-            wordFrequencies.sort((word, nextWord) -> nextWord.getWordCount() - word.getWordCount());
-
-            return  wordFrequencies.stream().map(wordFrequency -> wordFrequency.getWord() + " " + wordFrequency.getWordCount()).collect(Collectors.joining(LINE_BREAK));
+            List<WordFrequency> wordFrequenciesAggregated = aggregateWordFrequencies(wordToWordFrequencies);
+            return mapWordFrequenciesToString(wordFrequenciesAggregated);
         } catch (Exception e) {
             return CALCULATE_ERROR;
         }
+    }
+
+    private List<WordFrequency> mapSentenceToWordFrequencies(String sentences) {
+        return Arrays
+                .stream(sentences.split(SPACE))
+                .map(word -> new WordFrequency(word, 1))
+                .collect(Collectors.toList());
+    }
+
+    private List<WordFrequency> aggregateWordFrequencies(Map<String, List<WordFrequency>> wordToWordFrequencies) {
+        return wordToWordFrequencies
+                .entrySet()
+                .stream()
+                .sorted()
+                .map(entry -> new WordFrequency(entry.getKey(), entry.getValue().size()))
+                .collect(Collectors.toList());
+    }
+
+    private String mapWordFrequenciesToString(List<WordFrequency> wordFrequencies) {
+        return wordFrequencies
+                .stream()
+                .map(wordFrequency -> wordFrequency.getWord() + " " + wordFrequency.getWordCount())
+                .collect(Collectors.joining(LINE_BREAK));
     }
 
     private Map<String, List<WordFrequency>> getListMap(List<WordFrequency> wordFrequencyList) {
